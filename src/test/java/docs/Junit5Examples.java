d74e28bc6b48572e0bb2cb31694233b87fcd0ea1
pgujva <pgujva@usetech.ru>
2021-04-07 16:40:04 +0300

    config

--- src/test/java/docs/Junit5Examples.java
+++ src/test/java/docs/Junit5Examples.java
@@ -1,4 +1,32 @@
 package docs;
 
+import org.junit.jupiter.api.*;
+
 public class Junit5Examples {
+
+  @BeforeAll
+  static void setUp() {
+    System.out.println("setup here");
+  }
+
+  @BeforeEach
+  void each() {
+    System.out.println("    BeforeEach here");
+  }
+
+  @AfterEach
+  void after() {
+    System.out.println("    AfterEach here");
+  }
+
+  @Test
+
+  void someTest() {
+    System.out.println("        someTest here");
+  }
+
+  @Test
+  void anotherTest() {
+    System.out.println("        another here");
+  }
 }
